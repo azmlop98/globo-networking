0cfbce051abf8690c49dd4ef125c88e41e8c18e9
azmlop98 <matthew.tedesco.98@gmail.com>
2025-12-09 21:34:22 +0100

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,48 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-09e8dfdadb449a80c" #VPC
+# }
+# 
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-01e358f4d1e4638b3" #PublicSubnet1
+# }
+# 
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-067bcf973593ba10a" #PublicSubnet2
+# }
+# 
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-01a7808d30ad27ba2" #InternetGateway
+# }
+# 
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-08d390c80db24e628_0.0.0.0/0" #DefaultPublicRoute
+# }
+# 
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-08d390c80db24e628" #PublicRouteTable
+# }
+# 
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-01e358f4d1e4638b3/rtb-08d390c80db24e628" #PublicSubnet1/PublicRouteTable
+# }
+# 
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-067bcf973593ba10a/rtb-08d390c80db24e628" #PublicSubnet2/PublicRouteTable
+# }
+# 
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-0aabd212e3f1323d1" #NoIngressSecurityGroup
+# }
